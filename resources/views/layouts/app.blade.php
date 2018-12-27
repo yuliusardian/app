df2a3aa1d209edecac0968d8960d83dbb040fff5
Yulius Ardian Febrianto <yulius.febrianto@salt.co.id>
2018-12-27 11:46:01 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -293,6 +293,7 @@
 
     </div>
 
+    @if (Request::is('home'))
     <!-- Modal iklan -->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
@@ -313,6 +314,7 @@
             </div>
         </div>
     </div>
+    @endif
 
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/jquery.js') }}"></script>
